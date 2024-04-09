4063ec6b497f2243f03e0441e7913cc8689d620f
Zoé Bala <zoebala288@gmail.com>
2024-04-09 11:17:37 +0100

    importation deuxième version suite

--- app/Filament/Resources/EtudiantResource/Pages/ListEtudiants.php
+++ app/Filament/Resources/EtudiantResource/Pages/ListEtudiants.php
@@ -28,6 +28,7 @@ class ListEtudiants extends ListRecords
     {
         return [
             ImportAction::make("Importer")
+            ->label("Importer")
             ->icon("heroicon-o-users")
             // ->uniqueField('nom')
             ->fields([
@@ -50,56 +51,15 @@ class ListEtudiants extends ListRecords
                     ->label('Classe'),
 
             ]),
-            // Actions\CreateAction::make(),
-            // Actions\Action::make("importer")
-            // ->icon("heroicon-o-user-plus")
-            // ->form([
-            //     FileUpload::make("file"),
-            // ])->modalWidth(MaxWidth::Medium)
-            // ->modalIcon("heroicon-o-users")
-            // ->Action(function(Request $request){
-            //     // $file=request()->file('file');
 
-
-            //     Excel::import(new EtudiantsImport,$request->file('file'));;
-            //     Notification::make()
-            //     ->title('Importation effectuée avec succès')
-            //     ->success()
-            //      ->duration(5000)
-            //     ->send();
-
-            // }),
         ];
     }
-    // public function getHeader():?View
-    // {
-    //     $data=Actions\CreateAction::make();
-    //     return view("filament.custom.upload-file",compact("data"));
-    // }
 
     protected function getHeaderWidgets(): array
     {
         return [
             CreateEtudiantWidget::class,
         ];
-    }
-    #[Rule('required|file|mimes:csv:max:4048')]
-    public $file="";
-
-    public function save(){
-        if($this->file != ""){
-            Excel::import(new EtudiantsImport,$this->file);
-
-            Notification::make()
-                ->title('Importation effectuée avec succès')
-                ->success()
-                 ->duration(5000)
-                ->send();
-
-            // $this->file->reset();
-        }
-
-
     }
 
 
