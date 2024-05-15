4c28eaf43c9b948e78204bf7d5b13ba8ca7ba2a9
Zoé Bala <zoebala288@gmail.com>
2024-05-15 11:43:08 +0100

    fixing typo etat de sortie profil

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
 
     <div style="position: absolute; bottom:0px; left:0px;width:100%;">
 
-        <p style=" width:20%; display:inline-block; position:relative:bottom:2px; margin-left:10px;">printed by
+        <p style=" width:20%; display:inline-block; position:relative:bottom:2px; margin-left:20px;">printed by
             <img style="position:relative; top:10px;" src="{{'images/logo.jpeg'}}" alt="logo" width="50" class="img-fluid">
         </p>
         <p class="fst-italic" style="margin-left:45%; display:inline-block;position: relative;bottom:4px; "> Fait à Mbanza-Ngungu le {{$date}}</p>
