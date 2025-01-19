1c2626773afd2e39a0225d72b3042223926fa800
Zoé Bala <zoebala288@gmail.com>
2025-01-19 07:33:32 +0100

    répartition des états de sortie dans leurs resources respectives

--- app/Filament/Resources/PaiementResource/Pages/ListPaiements.php
+++ app/Filament/Resources/PaiementResource/Pages/ListPaiements.php
@@ -18,6 +18,9 @@ use Illuminate\Database\Eloquent\Builder;
 use App\Filament\Resources\PaiementResource;
 use Filament\Resources\Pages\ListRecords\Tab;
 use App\Filament\Resources\PaiementResource\Widgets\CreatePaiementWidget;
+use App\Models\Classe;
+use Filament\Actions\ActionGroup;
+use Filament\Forms\Components\TextInput;
 
 class ListPaiements extends ListRecords
 {
@@ -29,72 +32,149 @@ class ListPaiements extends ListRecords
             Actions\CreateAction::make()
             ->label("Effectuer Paiement")
             ->icon("heroicon-o-banknotes"),
+            ActionGroup::make([
+               
+                Action::make("étudiants ayant payé")
+                ->label("Liste des étudiants ayant payé")
+                ->form([
+                    Select::make("annee_id")
+                        ->label("Année Académique")
+                        ->options(Annee::whereId(session("Annee_id") ?? 1)->pluck("lib","id"))
+                        ->required()
+                        ->searchable(),
+                    Select::make("classe_id")
+                        ->label("Promotion")
+                        ->required()
+                        ->options(Classe::query()->pluck("lib","id"))
+                        ->searchable(),
+                    TextInput::make("Montant")
+                    ->label("Montant Payé au moins")
+                    ->placeholder("Ex: 200000")
+                    ->suffix("FC")
+
+
+                ])
+                ->button()
+                ->slideOver()
+                ->icon("heroicon-o-clipboard-document-list")
+                ->modalWidth(MaxWidth::Medium)
+                ->modalIcon("heroicon-o-users")
+                ->action(function(array $data){
+
+                    $annee_id=$data["annee_id"];
+                    $classe_id=$data["classe_id"];
+                    $montant=$data["Montant"];
+
+                    if(isset($montant)){
+
+                        return redirect()->route("etudiants.paye1",compact("annee_id","classe_id","montant"));
+                    }else{
+                        return redirect()->route("etudiants.paye",compact("annee_id","classe_id"));
+                    }
+                })
+                ->openUrlInNewTab()
+                ->tooltip("Liste des étudiants ayant payé")
+                ->button()
+                ->icon("heroicon-o-clipboard-document-list"),
+                Action::make("Liste de tous les frais payés")
+                    ->form([
+                        Select::make("annee_id")
+                            ->label("Année Académique")
+                            ->options(Annee::whereId(session("Annee_id") ?? 1)->pluck("lib","id"))
+                            ->required()
+                            ->searchable(),
+                    ])
+                    ->button()
+                    ->icon("heroicon-o-clipboard-document-list")
+                    ->modalWidth(MaxWidth::Medium)
+                    ->modalIcon("heroicon-o-users")
+                     ->hidden(fn():bool => !Auth()->user()->hasRole(["COMGER","Admin","SGACAD","SACAD","ADMIN_BUDGET","SGADMN","DG"]))
+                    ->action(function(array $data){
+
+                        $annee_id=$data["annee_id"];
+
+
+
+                        return redirect()->route("frais.paye",compact("annee_id"));
+                    })
+                    ->openUrlInNewTab()
+                    ->tooltip("Liste de tous les frais payés")
+                    ->button()
+                    ->icon("heroicon-o-clipboard-document-list")
+                    ->color("warning"),
+                Action::make("Liste des frais payés par promotion")
+                    ->form([
+                        Select::make("annee_id")
+                            ->label("Année Académique")
+                            ->options(Annee::whereId(session("Annee_id") ?? 1)->pluck("lib","id"))
+                            ->required()
+                            ->searchable(),
+                        Select::make("classe_id")
+                            ->label("Promotion")
+                            ->required()
+                            ->options(Classe::query()->pluck("lib","id"))
+                            ->searchable(),
+
+                    ])
+                    ->button()
+                    ->icon("heroicon-o-clipboard-document-list")
+                    ->modalWidth(MaxWidth::Medium)
+                    ->modalIcon("heroicon-o-users")
+                    ->action(function(array $data){
+
+                        $annee_id=$data["annee_id"];
+                        $classe_id=$data["classe_id"];
+
+
+                        return redirect()->route("frais_promotion",compact("annee_id","classe_id"));
+                    })
+                    ->openUrlInNewTab()
+                    ->tooltip("Listes des frais payés par promotion"),
+                Action::make("Liste_étudiants_en_ordre_première_tranche")
+                    ->form([
+                        Select::make("annee_id")
+                            ->label("Année Académique")
+                            ->options(Annee::whereId(session("Annee_id") ?? 1)->pluck("lib","id"))
+                            ->required()
+                            ->searchable(),
+                        Select::make("classe_id")
+                            ->label("Promotion")
+                            ->required()
+                            ->options(Classe::query()->pluck("lib","id"))
+                            ->searchable(),
+                        Select::make("etat")
+                            ->label("Situation")
+                            ->required()
+                            ->options([
+                                "En ordre"=>"En ordre",
+                                "Non en ordre"=>"Non en ordre",
+                            ])
+                            ->searchable(),
+
+                    ])
+                    ->button()
+                    ->hidden(fn():bool => !Auth()->user()->hasRole(["COMGER","Admin","ADMIN_BUDGET","SGADMN","DG"]))
+                    ->icon("heroicon-o-clipboard-document-list")
+                    ->modalWidth(MaxWidth::Small)
+                    ->modalIcon("heroicon-o-users")
+                    ->action(function(array $data){
+
+                        $annee_id=$data["annee_id"];
+                        $classe_id=$data["classe_id"];
+                        $etat=$data["etat"];
+
+
+                        return redirect()->route("etudiant_tranche",compact("annee_id","classe_id","etat"));
+                    })
+                    ->openUrlInNewTab()
+                    ->tooltip("Liste des étudiants en ordre avec la première tranche")
+                    ->color("warning"),
+
+            ]),
         ];
     }
 
-    public $defaultAction="Annee";
-    protected function Annee():Action
-    {
 
-       return Action::make("annee")
-        ->modalHeading(function(){
-            if(Auth()->user()->hasRole("CANDIDAT"))
-                return "Choisir une année Académique";
-            else
-                return "Définition de l'année de travail";
-        })
-        ->icon("heroicon-o-calendar-days")
-        ->visible(fn():bool => session("Annee_id")==null)
-        ->form([
-            Select::make("annee")
-            ->label("Choix de l'année")
-            ->searchable()
-            ->required()
-            ->live()
-            ->afterStateUpdated(function($state,Set $set){
-                $Annee=Annee::whereId($state)->get(["lib","debut"]);
-                $set("lib_annee",$Annee[0]->lib);
-                $set("annee_debut",$Annee[0]->debut);
-            })
-            ->options(Annee::query()->pluck("lib","id")),
-            Hidden::make("lib_annee")
-            ->label("Année Choisie")
-            ->dehydrated(true),
-            Hidden::make("annee_debut")
-            ->label("Année Début")
-            ->dehydrated(true),
-
-        ])
-        ->modalWidth(MaxWidth::Medium)
-        ->modalIcon("heroicon-o-calendar")
-        ->action(function(array $data){
-            if(session('Annee_id')==NULL && session('Annee')==NULL && session('AnneeDebut')==NULL){
-
-                session()->push("Annee_id", $data["annee"]);
-                session()->push("Annee", $data["lib_annee"]);
-                session()->push("AnneeDebut", $data["annee_debut"]);
-            }else{
-                session()->pull("Annee_id");
-                session()->pull("Annee");
-                session()->pull("AnneeDebut");
-                session()->push("Annee_id", $data["annee"]);
-                session()->push("Annee", $data["lib_annee"]);
-                session()->push("AnneeDebut", $data["annee_debut"]);
-
-            }
-
-            // dd(session('Annee'));
-            Notification::make()
-            ->title("Fixation de l'annee de travail en ".$data['lib_annee'])
-            ->success()
-            ->duration(5000)
-            ->send();
-
-            return redirect()->route("filament.admin.resources.frais.index");
-
-
-        });
-    }
 
     public function getTabs():array
     {
